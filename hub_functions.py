bccfaa32b14711302122d886ab5e30c416a1f4ef
Nithum Bandara <nithum.bandara@roboroos.org.au>
2023-10-07 12:33:40 +1030

    add hub functions

---
+++ hub_functions.py
@@ -0,0 +1,16 @@
+from pybricks.tools import wait
+
+def countdown(hub, count):
+    if count > 0:
+        if count < 10:
+            for i in reversed(range(1, count + 1)):
+                hub.display.char(str(i))
+                wait(1000)
+        elif count > 9 and count < 100:
+            for i in reversed(range(1, count + 1)):
+                hub.display.number(i)
+                wait(1000)
+        else:
+            hub.display.text("error", 500, 250)
+    else:
+        hub.display.text("error", 500, 250)
