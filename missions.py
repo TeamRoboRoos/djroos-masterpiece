19fda9324ef8f28d702d8644d9be1251293fc59e
Leng Lim <leng.lim@roboroos.org.au>
2023-10-22 17:32:05 +1030

    Change run 6

--- missions.py
+++ missions.py
@@ -1,6 +1,9 @@
 from pybricks.tools import wait
 
-from core import DEFAULT_WAIT_TIME, DEFAULT_DRIVE_SPEED
+from core import (
+    DEFAULT_WAIT_TIME,
+    DEFAULT_DRIVE_SPEED
+)
 
 from attachment_functions import *
 from core import Robot
@@ -32,7 +35,6 @@ def run_1B(robot: Robot):
     move_backwards_distance(robot, 300, 500)
     # rotate_left_attachment_motor(robot, -130, 500)
 
-
 def run_2(robot: Robot):
     """Mission 6 and 7"""
     robot.hub.imu.reset_heading(0)
@@ -40,7 +42,7 @@ def run_2(robot: Robot):
     wait(DEFAULT_WAIT_TIME)
     # turn(robot, -1)
     move_distance(robot, 40, 150)
-    # wait(DEFAULT_WAIT_TIME)
+    # wait(DEFAULT_WAIT_TIME) 
     turn(robot, 42)
     wait(100)
     move_distance(robot, 50)
@@ -48,7 +50,6 @@ def run_2(robot: Robot):
     move_backwards_distance(robot, 90)
     turn(robot, -45)
     move_backwards_distance(robot, 650, 500)
-
 
 def run_3(robot: Robot, Lreseting_angle=0, Rreseting_angle=0):
     """Mission 8 and rolling camara mission"""
@@ -67,19 +68,17 @@ def run_3(robot: Robot, Lreseting_angle=0, Rreseting_angle=0):
     move_distance(robot, 400)
     rotate_right_attachment_motor(robot, 120, 1000)
 
-
 def run_4(robot: Robot):
     """Robot driving from blue to red home base"""
     move_distance(robot, 400, DEFAULT_DRIVE_SPEED)
     wait(DEFAULT_WAIT_TIME)
-    move_distance(robot, 70)
+    drive_timed(robot, .75, 100)
     wait(DEFAULT_WAIT_TIME)
-    rotate_left_attachment_motor(robot, -200, speed=500)
-    move_distance(robot, 20)
+    rotate_left_attachment_motor(robot, -180, speed=500)
+    move_distance(robot, 50)
     wait(DEFAULT_WAIT_TIME)
-    move_backwards_distance(robot, 500, 250)
-    rotate_left_attachment_motor(robot, 200)
-
+    move_backwards_distance(robot, 550, 250)
+    rotate_left_attachment_motor(robot, 182)
 
 def run_5(robot: Robot):
     move_distance(robot, 380, 200)
@@ -89,31 +88,34 @@ def run_5(robot: Robot):
     turn(robot, -27)
     move_backwards_distance(robot, 380, 200)
 
-
 def run_6(robot: Robot):
     move_distance(robot, 330, 200)
     print(f"Heading is - {robot.hub.imu.heading()}")
     turn(robot, -45)
     move_distance(robot, 400, 200)
-    turn(robot, -23, 100)
-    move_distance(robot, 420, 200)
+    turn(robot, -24, 100)
+    move_distance(robot, 460, 200)
     rotate_left_attachment_motor(robot, -150, wait=True)
-    turn(robot, -7)
+    turn(robot, -5)
     wait(100)
-    rotate_left_attachment_motor(robot, 150)
     move_backwards_distance(robot, 110, speed=200)
     turn(robot, -75 )
     move_distance(robot, 300, 200)
-    rotate_right_attachment_motor(robot, 150, wait=True)
-    rotate_right_attachment_motor(robot, -150)
+    rotate_right_attachment_motor(robot, 120, 100, wait=True)
+    wait(100)
+    rotate_right_attachment_motor(robot, -45, 100)
+    turn(robot, 2)
+    move_distance(robot, 200, speed=150)
+    wait(250)
+    move_backwards_distance(robot, 40, 500)
+    move_distance(robot, 40, 500)
+    wait(250)
+    move_backwards_distance(robot, 40, 500)
     raise KeyboardInterrupt
-    # move_distance(robot, 40, 500)
-    # wait(1000)
-    # move_backwards_distance(robot, 40, 500)
     # rotate_left_attachment_motor(robot, 180, 1000, wait=True)
     # wait(1000)
     # rotate_left_attachment_motor(robot, -180, 1000)
-
+    
 
 def test(robot: Robot):
     move_distance(robot, 100)
